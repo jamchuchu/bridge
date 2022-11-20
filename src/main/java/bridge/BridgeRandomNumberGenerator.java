bb2834b5240f0db74447a2f3a2f47470f7cfa7e2
DESKTOP-6F3759O\hyemin <gpals5987@gmail.com>
2022-11-21 03:20:20 +0900

    다리 한칸 업 다운 랜덤 생성

---
+++ src/main/java/bridge/BridgeRandomNumberGenerator.java
@@ -0,0 +1,17 @@
+package bridge;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+
+public class BridgeRandomNumberGenerator implements BridgeNumberGenerator {
+
+    private static final int RANDOM_UPPER_INCLUSIVE = 1;
+    private static final int RANDOM_LOWER_INCLUSIVE = 0;
+
+    @Override
+    public int generate() {
+        return Randoms.pickNumberInRange(RANDOM_LOWER_INCLUSIVE, RANDOM_UPPER_INCLUSIVE);
+    }
+
+
+}
