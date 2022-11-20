962576c81fe363a0118e4d92090b4a9061197e87
DESKTOP-6F3759O\hyemin <gpals5987@gmail.com>
2022-11-21 03:21:16 +0900

    메인

---
+++ src/main/java/bridge/Application.java
@@ -0,0 +1,11 @@
+package bridge;
+
+
+
+public class Application{
+
+    public static void main(String[] args) {
+        GameControl gameControl = new GameControl();
+        gameControl.game();
+    }
+}
